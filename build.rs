177ae89b99b545ca3863218b88722a2661e2a3d0
Adam <sector-f@users.noreply.github.com>
2018-08-13 15:17:29 -0400

    First commit

---
+++ build.rs
@@ -0,0 +1,34 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    println!("cargo:rustc-link-lib=libvirt");
+
+    // # include <libvirt/libvirt-common.h>
+    // # include <libvirt/libvirt-host.h>
+    // # include <libvirt/libvirt-domain.h>
+    // # include <libvirt/libvirt-domain-snapshot.h>
+    // # include <libvirt/libvirt-event.h>
+    // # include <libvirt/libvirt-interface.h>
+    // # include <libvirt/libvirt-network.h>
+    // # include <libvirt/libvirt-nodedev.h>
+    // # include <libvirt/libvirt-nwfilter.h>
+    // # include <libvirt/libvirt-secret.h>
+    // # include <libvirt/libvirt-storage.h>
+    // # include <libvirt/libvirt-stream.h>
+
+        let bindings = bindgen::Builder::default()
+            .header_contents("libvirt.h", "#include <libvirt/libvirt.h>")
+            .generate()
+            .expect("Unable to generate bindings");
+
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+}
+
